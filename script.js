3ed74eee1f5ec97653778fd6817e95030fba0c64
Cliff <cliffwn@gmail.com>
2021-07-04 11:24:29 -0400

    added JS to allow for the slider to affect the grid size

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const containerDiv = document.getElementsByClassName("container");
-const etchBox = document.querySelector(".etchBox")
-const gridNumber = 16
+const etchBox = document.querySelector(".etchBox");
 
+const gridNumber = 16;
+const sliderValue = document.getElementById("gridSlider").value;
 
-for (let i = 0; i < (gridNumber ** 2); i++) {
-    let singleGridDiv = document.createElement("div");
-    singleGridDiv.setAttribute("class", "singleGridDiv");
-    etchBox.appendChild(singleGridDiv);
-}
+console.log(sliderValue);
+
+function etchBoxSize(gridSizeFromSlider) {
+    etchBox.setAttribute('style',
+        `grid-template-columns: repeat(${gridSizeFromSlider}, 1fr);\
+         grid-template-rows: repeat(${gridSizeFromSlider}, 1fr);`);
+
+    for (let i = 0; i < (gridSizeFromSlider ** 2); i++) {
+        let singleGridDiv = document.createElement("div");
+        singleGridDiv.setAttribute("class", "singleGridDiv");
+        etchBox.appendChild(singleGridDiv);
+    };
+};
+
+etchBoxSize(sliderValue);
